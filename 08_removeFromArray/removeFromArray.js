9d571c7f1c08974eb48021b8568286913383b9bf
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2025-06-16 09:30:20 +0800

    Completed the removeFromArray exercise from Odin Project

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...remove) {
+    let finalArr = [];
+
+    arr.forEach((item) => {
+        const index = remove.indexOf(item);
+        if (index === -1) {
+            finalArr.push(item);
+        }
+    });
+
+    return finalArr;
 };
 
 // Do not edit below this line
