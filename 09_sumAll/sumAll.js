c778339f1b52ed644c963e81778d4a8317128222
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2025-06-17 08:21:18 +0800

    Completed the exercise sumAll from Odin Project

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+
+    if (startNum > endNum) {
+        [startNum, endNum] = [endNum, startNum];
+    }
+
+    let sum = 0;
+    if (startNum >= 0 && endNum >= 0 && typeof startNum === "number" && typeof endNum === "number" && Number.isInteger(startNum) && Number.isInteger(endNum)) {
+        for (let i = startNum; i <= endNum; i++)
+        {
+            sum += i;
+        }
+            
+        return sum;
+    }
+    else {
+        return "ERROR";
+    }
 
 };
 
