e00dc592522e1b6215d0406c1cec6bb0e345ad51
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2025-06-30 08:08:08 +0800

    Completed the Palindromes exercise from the Odin Project

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,11 @@
-const palindromes = function () {
+const palindromes = function (sentence) {
+    let words = sentence.toLowerCase().replace(/[^a-z0-9]/gi, "");
 
+    let splitWords = words.split("");
+    let reverseWords = splitWords.reverse();
+    let joinWords = reverseWords.join("");
+
+    return joinWords === words;
 };
 
 // Do not edit below this line
