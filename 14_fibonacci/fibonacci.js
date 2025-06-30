3fa0dff3453dffb00cf50ccc0cbf54824f99c754
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2025-07-01 07:59:51 +0800

    Completed the FIbonacci task from the Odin Project

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    const fibLen = Number(num);
 
+    let fib = [0, 1];
+
+    for (let i = 2; i <= fibLen; i++) {
+        fib[i] = fib[i - 2] + fib[i - 1];
+    }
+
+    return (num < 0) ? "OOPS" : (fibLen === 0) ? 0 : fib[fib.length - 1];
 };
 
 // Do not edit below this line
