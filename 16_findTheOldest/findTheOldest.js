673bb3a548b1151004a8073089e628cdf3a469ed
Jarnrsvlnn <173128640+jarnrsvlnn@users.noreply.github.com>
2025-07-01 09:14:39 +0800

    Completed the last task named findTheOldest from the Odin Project

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(person) {
 
+    person.map(obj => {
+        obj.age = ("yearOfDeath" in obj) ? obj.yearOfDeath - obj.yearOfBirth : (2025 - obj.yearOfBirth); 
+    });
+
+    const oldestPerson = person.reduce((oldest, currentPerson) => {
+        
+        if (currentPerson.age > oldest.age) {
+            return currentPerson;
+        }
+        else {
+            return oldest;
+        }
+    }, person[0])
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
